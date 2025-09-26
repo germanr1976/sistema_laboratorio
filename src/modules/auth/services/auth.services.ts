a080dc512a5f4d431d327d0328606dfe72aefbdf
Jesús Ojeda <ojedajesus232@gmail.com>
2025-09-25 23:31:08 -0500

    feat(auth): add services/funtions auth basics

---
+++ src/modules/auth/services/auth.services.ts
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export async function hashPassword(password: string):Promise<string>{
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS || '12');
+    const passwordHashed = await bcrypt.hash(password,saltRounds);
+    return passwordHashed;
+} 
+
+export async function comparePassword(password:string,hashedPassword:string):Promise<boolean> {
+    const isPassEquals = await bcrypt.compare(password,hashedPassword);
+    return isPassEquals;
+}
+
+export async function generateToken(userData: {
+    userId: number,
+    dni: string,
+    roleId: number,
+    roleName: string
+}): Promise<string> {
+    const jwtSecret = process.env.JWT_SECRET || 'fallback-secret';
+    const expiresIn = process.env.JWT_EXPIRES_IN || '24h';
+    
+    const token = jwt.sign(userData, jwtSecret, { expiresIn } as jwt.SignOptions);
+    return token;
+}
