fe41bddea00314eb4457df382f06a52f72a3bf77
Jesús Ojeda <ojedajesus232@gmail.com>
2025-09-27 12:10:29 -0500

    feat(auth): add auth middleware

---
+++ src/modules/auth/middlewares/auth.middleware.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from '../services/auth.services';
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
+
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    try {
+        // 1. Extraer token del header
+        const authHeader = req.headers.authorization;
+        if(!authHeader){
+            return res.status(401).json({
+                success: false, 
+                message: 'Token no proporcionado'
+
+            });
+        }
+        if(!authHeader.startsWith('Bearer ')){
+            return res.status(401).json({
+                success: false,
+                message: 'Formato de token invalido'
+            })
+        }
+        const token = authHeader.substring(7); 
+        // 2. Verificar token
+        const tokenverify = await verifyToken(token);
+        if(!tokenverify){
+            return res.status(401).json({
+                success: false, 
+                message: 'Token expirado'
+            }); 
+        }
+        // 3. Buscar usuario en BD
+        const user = await prisma.user.findUnique({
+            where:{id: tokenverify.userId},
+            include:{profile: true, role:true}
+        })
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message:'usuario no encontrado' 
+            })
+        }
+        // 4. Agregar user al req
+        req.user = user
+        // 5. Llamar next()
+       return  next()
+    } catch (error) {
+        // Manejar errores
+        return res.status(401).json({ 
+            success: false, 
+            message: 'Token inválido' 
+        });
+    }
+}
